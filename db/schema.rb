3acaf2591294e290c030cd3d6bf90471227a20c9
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-06-24 14:52:06 -0600

    Add seed data for recipes and user

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_06_22_231151) do
     t.string "description"
     t.integer "avg_rating"
     t.integer "favourites"
+    t.integer "upvotes"
+    t.integer "downvotes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  username: 'johnsmith', 
+  firstname: 'John', 
+  lastname: 'Smith',
+  email: 'johnsmith@gmail.com',
+  password: 'password'
+)
+
+Recipe.create(
+  title: 'Hotdog Soup',
+  description: 'A delicious great depression style soup that feeds 5',
+  avg_rating: 3,
+  favourites: 1000,
+  upvotes: 450,
+  downvotes: 35
+)
+
+Recipe.create(
+  title: 'Seaweed Salad',
+  description: 'Combines 3 types of seaweed with a nice italian dressing',
+  avg_rating: 4,
+  favourites: 233,
+  upvotes: 320,
+  downvotes: 10
+)
+
+Recipe.create(
+  title: 'Roast Beast',
+  description: 'This recipe combines your two favourite food groups: beef and salt',
+  avg_rating: 2,
+  favourites: 100,
+  upvotes: 12,
+  downvotes: 102
+)
